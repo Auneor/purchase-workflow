b34bf682eb0785fa22384009f2faeed2be2094b7
Alpha Oumar <alpha.oumar@auneor-conseil>
2025-04-16 16:01:10 +0200

    R-48138: improve filter to search module

--- purchase_quick/models/product_product.py
+++ purchase_quick/models/product_product.py
@@ -6,6 +6,7 @@
 
 
 from odoo import api, fields, models
+from odoo.osv import expression
 
 
 class ProductProduct(models.Model):
@@ -64,21 +65,28 @@ class ProductProduct(models.Model):
     @api.model
     def search(self, args, offset=0, limit=None, order=None, count=False):
         purchase = self.env["purchase.order"].browse(self.env.context.get("parent_id"))
+
+        in_current_parent_domain = []
+        for_current_supplier_domain = []
+
         if self.env.context.get("in_current_parent") and purchase:
             po_lines = self.env["purchase.order.line"].search(
                 [("order_id", "=", purchase.id)]
             )
-            args.append(("id", "in", po_lines.mapped("product_id").ids))
+            in_current_parent_domain.append(("id", "in", po_lines.mapped("product_id").ids))
         if self.env.context.get("for_current_supplier") and purchase:
             seller = purchase.partner_id
             seller = seller.commercial_partner_id or seller
-            args += [
+            for_current_supplier_domain += [
                 "|",
                 ("variant_specific_seller_ids.partner_id", "=", seller.id),
                 "&",
                 ("seller_ids.partner_id", "=", seller.id),
                 ("product_variant_ids", "!=", False),
             ]
+
+        args += expression.OR([in_current_parent_domain, for_current_supplier_domain])
+
         return super().search(
             args, offset=offset, limit=limit, order=order, count=count
         )
